0b92ae3a4f404b8487b5f2c5e7bd75a0bbe3bc79
Martin Paul <martin.paul@vogler-engineering.de>
2024-10-28 11:09:55 +0100

    added a field

--- src/impl/WeatherModelPhoneObserver.java
+++ src/impl/WeatherModelPhoneObserver.java
@@ -4,8 +4,12 @@ import models.WeatherModel;
 import pattern.IObserver;
 
 public class WeatherModelPhoneObserver implements IObserver<WeatherModel> {
+    private WeatherModel lastWeatherModel;
+
     @Override
     public void onNext(WeatherModel weatherModel) {
+        lastWeatherModel = weatherModel;
+
         System.out.printf("📱 %s received new WeatherData { time: %d temperature: %f description: %s }\n", this.getClass().getSimpleName(), weatherModel.time, weatherModel.temperature, weatherModel.description);
     }
 
