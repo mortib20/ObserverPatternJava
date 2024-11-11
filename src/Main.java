16112f9c1adc8721aa3a570c0094136b37044c6b
Martin Paul <martin.paul@vogler-engineering.de>
2024-11-11 10:45:55 +0100

    final

--- src/Main.java
+++ src/Main.java
@@ -5,22 +5,34 @@ import models.WeatherModel;
 
 public class Main {
     public static void main(String[] args) {
+        // Instanziieren des Observable
         var weatherObservable = new WeatherModelObservable();
 
+        // Instanziieren der Observer
         var phoneObserver = new WeatherModelPhoneObserver();
         var computerObserver = new WeatherModelComputerObserver();
 
+        // Abonnieren der Beobachter
         weatherObservable.subscribe(phoneObserver);
         weatherObservable.subscribe(computerObserver);
 
+        // Neue Informationen an die Observables
         weatherObservable.updateWeatherModel(WeatherModel.getRandomWeatherModel());
         weatherObservable.updateWeatherModel(WeatherModel.getRandomWeatherModel());
         weatherObservable.updateWeatherModel(WeatherModel.getRandomWeatherModel());
         weatherObservable.updateWeatherModel(WeatherModel.getRandomWeatherModel());
 
+        // PhoneObserver wird nicht mehr informiert
         weatherObservable.unsubscribe(phoneObserver);
+
+        weatherObservable.updateWeatherModel(WeatherModel.getRandomWeatherModel());
+
         weatherObservable.unsubscribe(computerObserver);
 
-        System.out.println();
+        // Keine Meldungen mehr in der Konsole
+        System.out.println("Nur der Computer wird informiert.");
+        weatherObservable.updateWeatherModel(WeatherModel.getRandomWeatherModel());
+
+        System.out.println("Ende des Codes...");
     }
 }
